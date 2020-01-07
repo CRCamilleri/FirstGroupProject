2fc29070c16fbe63cf5afc789ca7136f870b063b
Matthew Wester <MatthewWester@live.com>
2020-01-07 15:20:09 -0700

    Recipe Book and Grocery List pages show saved recipe information

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ var queriedRecipe7 = [];
 var queriedRecipe8 = [];
 var queriedRecipe9 = [];
 
+//On Index page this function takes search bar input and gives ten results for recipes
 function createRecipe() {
-    //This next line is the food that the user searches for
     var mealName = $("#recipeInput").val().trim();
     $.ajax({
         url: "https://api.edamam.com/search?q=" + mealName + "&app_id=70e00e26&app_key=6c683b56a399b435d00ee3100c0ca055",
@@ -18,15 +18,15 @@ function createRecipe() {
     }).then(function (response) {
         for (i = 0; i < response.hits.length; i++) {
             var newDiv = $("#recipeReveal");
-        newDiv.append("<div class='recipe" + (i) + "'></div>")
-        .append("<p>Recipe result " + (i + 1) + ": " + response.hits[i].recipe.label + "</p>")
-            .append("<img src=" + response.hits[i].recipe.image + " alt='Recipe picture'>")
-            .append("Full recipe: " + response.hits[i].recipe.url);
-        //This for loop iterates through the ingredients for this recipe
-        for (j = 0; j < response.hits[i].recipe.ingredients.length; j++) {
-            $('#recipeReveal').append("<p>" + response.hits[i].recipe.ingredients[j].text + "</p>")
-        }
-        newDiv.append("<button id='result" + (i+1) + "' onclick='saveRecipe(" + (i) + ")'>Save Recipe</button>");
+            newDiv.append("<div class='recipe" + (i) + "'></div>")
+                .append("<p>Recipe result " + (i + 1) + ": " + response.hits[i].recipe.label + "</p>")
+                .append("<img src=" + response.hits[i].recipe.image + " alt='Recipe picture'>")
+                .append("Full recipe: " + response.hits[i].recipe.url);
+            //This for loop iterates through the ingredients for this recipe
+            for (j = 0; j < response.hits[i].recipe.ingredients.length; j++) {
+                $('#recipeReveal').append("<p>" + response.hits[i].recipe.ingredients[j].text + "</p>")
+            }
+            newDiv.append("<button id='result" + (i + 1) + "' onclick='saveRecipe(" + (i) + ")'>Save Recipe</button>");
         }
         queriedRecipe0 = (response.hits[0].recipe.uri);
         queriedRecipe1 = (response.hits[1].recipe.uri);
@@ -42,7 +42,7 @@ function createRecipe() {
 }
 
 function saveRecipe(number) {
-    if (localStorage.getItem("savedRecipes") ==  null) {
+    if (localStorage.getItem("savedRecipes") == null) {
         if (number == 0) {
             localStorage.setItem("savedRecipes", JSON.stringify(queriedRecipe0));
         } else if (number == 1) {
@@ -63,7 +63,8 @@ function saveRecipe(number) {
             localStorage.setItem("savedRecipes", JSON.stringify(queriedRecipe8));
         } else if (number == 9) {
             localStorage.setItem("savedRecipes", JSON.stringify(queriedRecipe9));
-        }}
+        }
+    }
     else {
         let currentResults = localStorage.getItem("savedRecipes");
         if (number == 0) {
@@ -71,49 +72,70 @@ function saveRecipe(number) {
         } else if (number == 1) {
             localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe1));
         } else if (number == 2) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe2));
+            localStorage.setItem("savedRecipes", currentResults +  JSON.stringify(queriedRecipe2));
         } else if (number == 3) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe3));
+            localStorage.setItem("savedRecipes", currentResults +  JSON.stringify(queriedRecipe3));
         } else if (number == 4) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe4));
+            localStorage.setItem("savedRecipes", currentResults +  JSON.stringify(queriedRecipe4));
         } else if (number == 5) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe5));
+            localStorage.setItem("savedRecipes", currentResults +  JSON.stringify(queriedRecipe5));
         } else if (number == 6) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe6));
+            localStorage.setItem("savedRecipes", currentResults +  JSON.stringify(queriedRecipe6));
         } else if (number == 7) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe7));
+            localStorage.setItem("savedRecipes", currentResults +  JSON.stringify(queriedRecipe7));
         } else if (number == 8) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe8));
+            localStorage.setItem("savedRecipes", currentResults +  JSON.stringify(queriedRecipe8));
         } else if (number == 9) {
-            localStorage.setItem("savedRecipes", currentResults + JSON.stringify(queriedRecipe9));
+            localStorage.setItem("savedRecipes", currentResults  +  JSON.stringify(queriedRecipe9));
         }
     }
 }
 
 function recipeBookReveal() {
-    var savedRecipesString = localStorage.getItem("savedRecipes").split("http://www.edamam.com/ontologies/edamam.owl#recipe_");
-    for (i=1;i<savedRecipesString.length;i++) {
+    var recipeBookStorage = localStorage.getItem("savedRecipes");
+    recipeBookStorage = recipeBookStorage.replace(/\"/g, "");
+    recipeBookStorage = recipeBookStorage.replace(/\,/g, "");
+    recipeBookStorage = recipeBookStorage.split("http://www.edamam.com/ontologies/edamam.owl#recipe_");
+
+    for (i = 0; i < recipeBookStorage.length; i++) {
         $.ajax({
-            url: "https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_" + savedRecipesString[i] + "&app_id=70e00e26&app_key=6c683b56a399b435d00ee3100c0ca055",
+            url: "https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_" + recipeBookStorage[i] + "&app_id=70e00e26&app_key=6c683b56a399b435d00ee3100c0ca055",
             method: "GET"
         }).then(function (response) {
-            console.log(response);
-            $("#savedRecipesDumpHere").append("<p>" + response.label + "</p>")
-            .append("<p>" + response.label + "</p>")
-            .append("<p>" + response.image + "</p>")
-            .append("<p>" + response.url + "</p>");
-            for (j = 0; j < response.recipe.ingredients.length; j++) {
-                $('#savedRecipesDumpHere').append("<p>" + response.recipe.ingredients[j].text + "</p>")
+            $("#savedRecipesDumpHere").append("<p>" + response[0].label + "</p>")
+                .append("<img src=" + response[0].image + " alt='Recipe picture'>")
+                .append("<p>" + response[0].url + "</p>");
+            for (j = 0; j < response[0].ingredients.length; j++) {
+                $('#savedRecipesDumpHere').append("<p>" + response[0].ingredients[j].text + "</p>");
             }
-            $("#savedRecipesDumpHere").append("<button id='resultRecipeBook" + (i+1) + "' onclick='removeRecipe(" + (i) + ")'>Remove Recipe</button>")
+            $("#savedRecipesDumpHere").append("<button onclick='removeRecipe()'>Remove above recipe from Recipe Book</button>");
         })
     }
 }
 
 function removeRecipe() {
-    //will put function here that removes recipe from Recipe Book and localStorage
+    console.log("This button doesn't work yet!");
+    // var tweakThis = localStorage.getItem("savedRecipes");
+    // tweakThis.replace("http://www.edamam.com/ontologies/edamam.owl#recipe_" + address, '');
+    // $("#savedRecipesDumpHere").empty();
+    // recipeBookReveal();
 }
 
-$(document).ready(function() {
-    console.log("Page ready");
-  });
+function groceryListReveal() {
+    var recipeBookStorage = localStorage.getItem("savedRecipes");
+    recipeBookStorage = recipeBookStorage.replace(/\"/g, "");
+    recipeBookStorage = recipeBookStorage.replace(/\,/g, "");
+    recipeBookStorage = recipeBookStorage.split("http://www.edamam.com/ontologies/edamam.owl#recipe_");
+
+    for (i = 0; i < recipeBookStorage.length; i++) {
+        $.ajax({
+            url: "https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_" + recipeBookStorage[i] + "&app_id=70e00e26&app_key=6c683b56a399b435d00ee3100c0ca055",
+            method: "GET"
+        }).then(function (response) {
+            for (j = 0; j < response[0].ingredients.length; j++) {
+                $('#ingredientsListDumpsHere').append("<p>" + response[0].ingredients[j].text + " (" + response[0].label + ")</p>");
+            }
+        })
+    }
+    //put function to put all ingredients into grocery list
+}
